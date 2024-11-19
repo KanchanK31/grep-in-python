63c434b37399b9f1ae506d1d42f44cb3656742fa
Kanchan Kuwar <kanchan.kuwar@ignitesol.com>
2024-11-19 22:36:32 +0530

    added ignore case & number lineflags

--- mygrep.py
+++ mygrep.py
@@ -1,34 +1,55 @@
 import sys
 import os
 
-def grep(pattern,file_name):
+def print_match(flags,line,i):
+	if('-n' in flags):
+		print(f"{i+1}:{line}")
+	else:
+		print(f"{line}")
+
+
+def grep(pattern,file_name,flags):
 	try:
 		with open(file_name,'r') as file:
 			lines = file.readlines()
 			for i,line in enumerate(lines):
-				if(pattern.lower() in line.lower()):
-					print(f"{i+1}:{line}")
+				_line = line
+
+				if('-i' in flags):
+					pattern = pattern.lower()
+					_line = line.lower()
+
+
+				if(pattern in _line):
+					print_match(flags,line,i)
+
+
 	except Exception as e:
 		print("Unable to read file.Please provide valid file")
 		exit(0)
 
 def parse_args():
-	pattern = sys.argv[1]
-	file_name = sys.argv[2]
+	flags ,non_flags = [], []
 
+	for i in sys.argv:
+		if(i[0] == '-'):
+			flags.append(i)
+		else:
+			non_flags.append(i)
 
-	if(len(sys.argv)<3):
-		print("Not Enough Arguments.Please provide pattern and file path")
-		sys.exit(0)
+	if(len(non_flags)<3):
+		print("Not Enough Arguments")
+		exit(0)
 
-
+	pattern = non_flags[1]
+	file_name = non_flags[2]
 	isExist = os.path.exists(file_name) 
 
 	if(not isExist):
 		print("Please provide valid file path")
-		exit(0)
-	return pattern,file_name
+		exit(0) 
+	return (pattern,file_name,flags)
 
-if __name__=='__main__':
-	pattern,file_name = parse_args()
-	grep(pattern,file_name)
+if __name__ == '__main__':
+	pattern,file_name,flags = parse_args()
+	grep(pattern,file_name,flags)
