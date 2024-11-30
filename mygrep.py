b9b24ff3a8b5360e47b0c8f729a9dd57a64b4629
Kanchan Kuwar <kanchan.kuwar@ignitesol.com>
2024-11-30 17:32:54 +0530

    added directory fuctionality

--- mygrep.py
+++ mygrep.py
@@ -1,14 +1,17 @@
 import sys
 import os
 
-def print_match(flags,line,i):
+def print_match(flags,line,i,file_name):
+	result=""
 	if('-n' in flags):
-		print(f"{i+1}:{line}")
-	else:
-		print(f"{line}")
+		result+=f"{i+1}"
 
+	if(not '-h' in flags):
+		result+=f" {file_name}"
+	result+=f" {line}"
+	print(result)
 
-def grep(pattern,file_name,flags):
+def grep(pattern,file_name,flags,from_dir):
 	try:
 		with open(file_name,'r') as file:
 			lines = file.readlines()
@@ -21,12 +24,13 @@ def grep(pattern,file_name,flags):
 
 
 				if(pattern in _line):
-					print_match(flags,line,i)
+					print_match(flags,line,i,file_name)
 
 
 	except Exception as e:
-		print("Unable to read file.Please provide valid file")
-		exit(0)
+		if(not from_dir):
+			print(f"Unable to read file.Please provide valid file: {e} {file_name}")
+		# exit(0)
 
 def parse_args():
 	flags ,non_flags = [], []
@@ -50,6 +54,18 @@ def parse_args():
 		exit(0) 
 	return (pattern,file_name,flags)
 
+def process_file_path(pattern,file_name,flags):
+	if os.path.isfile(file_name):
+		grep(pattern,file_name,flags,from_dir=False)
+	else:
+		files = os.listdir(file_name)
+		for f in files:
+			f=os.path.join(file_name,f)
+			if os.path.isfile(f):
+				grep(pattern,f,flags,from_dir=True)
+			else:
+				process_file_path(pattern,f,flags)
+
 if __name__ == '__main__':
 	pattern,file_name,flags = parse_args()
-	grep(pattern,file_name,flags)
+	process_file_path(pattern,file_name,flags)
